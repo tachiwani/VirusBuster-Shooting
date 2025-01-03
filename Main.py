7105a45c9a593b7c54525866a3212eb8b8c5fc4e
tachiwani <41537984+tachiwani@users.noreply.github.com>
2025-01-03 22:08:49 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -251,7 +251,7 @@ class App:
         self.game_status = 0 #0は難易度選択画面　1はゲーム画面
         self.difficulty = 0 #0はeasy 1はnomal 2はhard
 
-        pyxel.init(WIDTH, HEIGHT, caption="VirusBuster_Shoting", quit_key=None)
+        pyxel.init(WIDTH, HEIGHT, caption="VirusBuster_Shoting", quit_key=None, mouse_visible=False)
         pyxel.logo(visible=False)  # ロゴ画面をスキップ
         pyxel.load('Characters.pyxres')
 
